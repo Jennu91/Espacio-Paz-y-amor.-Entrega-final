8441caf941af0cd09687bc3f6ea78bef2fc1a7bd
Martínez <cristian.a.martinez@accenture.com>
2022-02-03 14:51:45 -0300

    Arreglando algoritmo

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,38 +1,36 @@
 class Productos{
-    constructor(nombre, _talle, precio){
+    constructor(nombre, talle, precio){
         this.nombre = nombre;
-        this.pesotalle = peso;
+        this.talle = talle;
         this.precio = precio;        
     }
 
     mostrarInformacion(){
-        console.log("El nombre del producto es "+this.nombre+" el talle es "+this._talle+". "+"Su precio es de "+this.precio+");
+        console.log("El nombre del producto es "+this.nombre+" el talle es "+this.talle+". "+"Su precio es de "+this.precio);
     }
     
     ingresoDatos(){
         let nuevoNombre = prompt("Ingrese el nombre del producto");
         this.nombre=nuevoNombre;
         let nuevoTalle = prompt("Ingrese el talle del producto");
-        this._talle=nuevoTalle;
+        this.talle=nuevoTalle;
         let nuevoPrecio=prompt("Ingrese el precio del producto");
         this.precio=nuevoPrecio;       
     }
 }
 
-const productos1=new Productos();
-//productos1.ingresoDatos();
-//productos1.mostrarInformacion();
-
 const productosArray = [];
 let cantidad = prompt("Ingrese la cantidad de productos que desea agregar");
 
-do{
-    productos1.ingresoDatos();
-
-    productosArray.push(productos1);
+while(productosArray.length < cantidad){
+    const producto = new Productos();
+    producto.ingresoDatos();
+    console.log('productos ingresados');
+    productosArray.push(producto);
     console.log(productosArray.length);
-    productos1.mostrarInformacion();
-}while(productosArray.length != cantidad)
+    producto.mostrarInformacion();
+    console.log(productosArray)
+}
 
 
 
